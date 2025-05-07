3e5d911fc68336fc5869ee9c77456ef284f39ff6
WobblyWaffle98 <123881285+WobblyWaffle98@users.noreply.github.com>
2025-05-07 07:36:19 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from google import genai
+
+client = genai.Client(api_key="AIzaSyA0jZkj5buSGm6AXtXlo6CEeFS1f8q0KSg")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write(response.text)
