7267d9cc0d3291890f589f6fb8f41e5ccf518f68
WobblyWaffle98 <123881285+WobblyWaffle98@users.noreply.github.com>
2025-05-07 07:43:55 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,28 @@
 import streamlit as st
+
 from google import genai
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
 
 client = genai.Client(api_key="AIzaSyA0jZkj5buSGm6AXtXlo6CEeFS1f8q0KSg")
+model_id = "gemini-2.0-flash"
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="What happened to Brent oil over the past week"
+google_search_tool = Tool(
+    google_search = GoogleSearch()
 )
 
-st.title("Starsigns")
+response = client.models.generate_content(
+    model=model_id,
+    contents="When is the next total solar eclipse in the United States?",
+    config=GenerateContentConfig(
+        tools=[google_search_tool],
+        response_modalities=["TEXT"],
+    )
+)
 
-st.write(response.text)
+for each in response.candidates[0].content.parts:
+    print(each.text)
+# Example response:
+# The next total solar eclipse visible in the contiguous United States will be on ...
+
+# To get grounding metadata as web content.
+st.write_stream(response.candidates[0].grounding_metadata.search_entry_point.rendered_content)
